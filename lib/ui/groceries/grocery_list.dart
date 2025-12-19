26567eda0e7e4f01d758a7571d70541d251b1b75
Visal KONG <viisall59@gmail.com>
2025-12-19 14:01:15 +0700

    hx

---
+++ lib/ui/groceries/grocery_list.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import '../../data/mock_grocery_repository.dart';
+import '../../models/grocery.dart';
+import 'grocery_form.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  late List<Grocery> _groceryItems = List.from(dummyGroceryItems);
+
+  void onCreate() async {
+    final newGrocery = await Navigator.of(context).push<Grocery>(
+      MaterialPageRoute(
+        builder: (context) => const NewItem(),
+      ),
+    );
+
+    if (newGrocery != null) {
+      setState(() {
+        _groceryItems.add(newGrocery);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = const Center(child: Text('No items added yet.'));
+
+    if (_groceryItems.isNotEmpty) {
+      //  Display groceries with an Item builder and  LIst Tile
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) => GroceryItem(grocery: _groceryItems[index],),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: onCreate, icon: const Icon(Icons.add))],
+      ),
+      body: content,
+    );
+  }
+}
+
+class GroceryItem extends StatelessWidget {
+  const GroceryItem({super.key, required this.grocery});
+
+  final Grocery grocery;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(color: grocery.category.color, width: 15, height: 15,),
+      title: Text(grocery.name),
+      trailing: Text(grocery.quantity.toString()),
+    );
+  }
+}
